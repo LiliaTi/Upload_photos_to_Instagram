132cc9676ba217eb65230c0159be506b9fd34a91
LiliaTi <75118256+LiliaTi@users.noreply.github.com>
2020-12-14 13:08:11 +0300

    Update fetch_spacex.py

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -4,14 +4,17 @@ from common_functions import load_image, prepare_image_for_instagram
 
 
 def fetch_spacex_last_launch(path):
-    SpaceX_url = 'https://api.spacexdata.com/v4/launches/latest'
-    response = requests.get(SpaceX_url)
+    spacex_url = 'https://api.spacexdata.com/v4/launches/latest'
+    response = requests.get(spacex_url)
     response.raise_for_status()
     spacex_links = response.json()['links']['flickr']['original']
-    for link_number, link in enumerate(spacex_links):
-        filename = 'spacex{}.jpg'.format(link_number + 1)
-        load_image(filename, link, path)
-        prepare_image_for_instagram(filename, path)
+    if not spacex_links:
+        print('Фотографий при запуске не делали')
+    else:
+        for link_number, link in enumerate(spacex_links, start=1):
+            filename = 'spacex{}.jpg'.format(link_number)
+            load_image(filename, link, path)
+            prepare_image_for_instagram(filename, path)
 
 
 if __name__ == '__main__':
